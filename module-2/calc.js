4c93f23612133531a1af62124e8daa9ecfe0b672
Laszlo Szikszai <sziklaszlo@gmail.com>
2017-10-10 22:46:46 +0200

    Added Module 2 task

---
+++ module-2/calc.js
@@ -0,0 +1,54 @@
+'use strict';
+
+class Calc {
+    constructor(v) {
+        this.v = +v;
+    }
+
+    add(n) {
+        this.v += n;
+        return this;
+    }
+
+    minus(n) {
+        this.v -= n;
+        return this;
+    }
+
+    times(n) {
+        this.v *= n;
+        return this;
+    }
+
+    div(n) {
+        if (n === 0) {
+            throw new Error('Division by 0 is not possible!');
+        }
+        this.v /= n;
+        return this;
+    }
+
+    modulo(n) {
+        this.v %= n;
+        return this;
+    }
+
+    toString() {
+        return String(this.v);
+    }
+}
+
+function calc(n) {
+    return new Calc(n);
+}
+
+module.exports = calc;
+
+//console.log(calc(3).v);
+//console.log(calc(3).add(4).v);
+//console.log(calc(4).minus(5).v);
+//console.log(calc(4).times(6).v);
+//console.log(calc(10).div(5).v);
+//console.log(calc(10).modulo(5).v);
+//console.log(calc(4).add(4).times(5).div(1.5).minus(2).v);
+//console.log(calc(5).div(0));
