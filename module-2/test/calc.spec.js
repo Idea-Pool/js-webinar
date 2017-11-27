d88b0a502b6554f0bbc4347cd5b5a8840ec9ee77
Laszlo, Szikszai <laszlo_szikszai@epam.com>
2017-11-27 17:20:09 +0100

    Added Calc test barebone script

---
+++ module-2/test/calc.spec.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const calc = require('../calc');
+
+describe('calc', () => {
+    /*
+     * calc(3).v // 3
+     * calc(3).add(5).v // 8
+     * calc(3).minus(2).v // 1
+     * calc(4).sqrt().v // 2
+     * calc(3).times(10).v // 30
+     * calc(10).divide(2).v // 5
+     * calc(10).mod(5).v // 0
+     * calc(5).divide(0) // throw error
+     * calc(3).add(4)
+     *     .minus(3)
+     *     .times(6).v // 24
+     */
+    // TODO: write test cases to test calculator
+});
