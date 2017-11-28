d18c8f81322c141bb2f6a6d82b23a0d1e2f42702
Laszlo, Szikszai <laszlo_szikszai@epam.com>
2017-11-28 08:58:01 +0100

    Module2, guide

--- module-1/fibonacci.js
+++ module-1/fibonacci.js
@@ -19,13 +19,6 @@ function fibonacci(n) {
      */
     // PLACE YOUR CODE BETWEEN THIS...
 
-    if (n < 0) {
-        return 0;
-    }
-    if (n < 2) {
-        return n;
-    }
-    return fibonacci(n-1) + fibonacci(n-2);
 
     // ...AND THIS COMMENT LINE!
     return nThFibonacci;
