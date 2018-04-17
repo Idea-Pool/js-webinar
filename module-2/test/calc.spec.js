290b325a6c5a9774d423d1a46e54caff1244e36c
szikszail <sziklaszlo@gmail.com>
2018-04-17 18:22:37 +0200

    Cleanup

--- module-2/test/calc.spec.js
+++ module-2/test/calc.spec.js
@@ -11,7 +11,7 @@ describe.only('calc', () => {
      * calc(4).sqrt().v // 2
      * calc(3).times(10).v // 30
      * calc(10).divide(2).v // 5
-     * calc(10).mod(5).v // 0
+     * calc(10).modulo(5).v // 0
      * calc(5).divide(0) // throw error
      * calc(-3).sqrt() // throw error
      * calc(3).add(4)
@@ -20,30 +20,4 @@ describe.only('calc', () => {
      */
     // TODO: write test cases to test calculator
 
-    it('should have proper initial value', () => {
-        const c = calc(3);
-        expect(c.v).to.equal(3);
-    });
-
-    it('should support add operation', () => {
-        // Given
-        let c = calc(3);
-        // When
-        c = c.add(5);
-        // Then
-        expect(c.v).to.equal(8);
-    });
-
-    it('should support substract operation');
-
-    //...
-
-    it('should throw error in case of division by 0', () => {
-        // Given
-        let c = calc(3);
-        // When + Then
-        expect(() => c.divide(0)).to.throw;
-    });
-
-    //...
 });
--- module-5/features/EPAM.feature
+++ module-5/features/EPAM.feature
@@ -4,9 +4,4 @@ Feature: EPAM site
     So that I can practice Cucumber
 
     Scenario: Search for a job
-    Given EPAM Career site is opened
-    Then EPAM Career site should be opened
-    And The search form should be visible
-    And The non existing element should be hidden
-
     # TODO
