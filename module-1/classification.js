cce5002331e3c333d3aee5f62ce66b2af98dd0b2
Aliaksandr_Khatsko <Aliaksandr_Khatsko@epam.com>
2018-04-25 11:18:13 +0300

    Add implementation for Module1 - task 1

--- module-1/classification.js
+++ module-1/classification.js
@@ -13,18 +13,33 @@
  * @returns {number} grade or 0 if any arguments are not proper
  */
 function grade(score) {
-    let gradeOfStudent;
-    /*
-     * Your task is to calculate the grade of the student
-     * based on his/her score which can be found in the
-     * score variable and those rules can be found in the
-     * documentation of the function.
-     * Store the grade in the gradeOfStudent variable.
-     * Also take into consideration the documentation of the function!
-     */
-    // PLACE YOUR CODE BETWEEN THIS...
-
-    // ...AND THIS COMMENT LINE!
-    return gradeOfStudent;
+  let gradeOfStudent;
+  /*
+   * Your task is to calculate the grade of the student
+   * based on his/her score which can be found in the
+   * score variable and those rules can be found in the
+   * documentation of the function.
+   * Store the grade in the gradeOfStudent variable.
+   * Also take into consideration the documentation of the function!
+   */
+  // PLACE YOUR CODE BETWEEN THIS...
+  gradeOfStudent = 0;
+  if (score >= 90 && score <= 100) {
+    gradeOfStudent = 5;
+  }
+  if (score >= 80 && score <= 89) {
+    gradeOfStudent = 4;
+  }
+  if (score >= 70 && score <= 79) {
+    gradeOfStudent = 3;
+  }
+  if (score >= 60 && score <= 69) {
+    gradeOfStudent = 2;
+  }
+  if (score >= 0 && score <= 59) {
+    gradeOfStudent = 1;
+  }
+  // ...AND THIS COMMENT LINE!
+  return gradeOfStudent;
 }
 module.exports = grade;
