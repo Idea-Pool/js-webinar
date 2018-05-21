61ec0768979c2a9d90a02e52433d80db5b470fbf
Lusine Hakobyan <lusine_hakobyan@epam.com>
2018-05-21 21:26:09 +0400

    Add homework for module-4

--- module-4/arraySorted.js
+++ module-4/arraySorted.js
@@ -13,3 +13,31 @@
  * @returns {boolean} true if the array is properly sorted,
  *                    false otherwise
  */
+
+const ignore = /\ |\!|\?|\@|\#|\$|\%|\^|\&|\*|\(|\)|\_/g
+
+ function arraySorted(items) {
+     let result = true;
+     if (items.length === 0) {
+         return true;
+     }
+     let trimmedArray = items.map(function (n) {
+         if (typeof n === "string") {
+            if(n.match(ignore) !== -1) {
+               return n = n.replace(ignore, '').toLowerCase();
+            }
+            return n.toLowerCase();
+         } 
+         return n;
+    });
+    let expected = trimmedArray.slice(0).sort();
+    
+    expected.forEach((element, index) => {
+       if(element !== trimmedArray[index]) {
+           result = false;
+       }
+    });
+    return result;
+ }
+
+ module.exports = arraySorted;
