532ab167805d8bb5a1925e69e665f0433a6af8a8
milan.horvath <Milan.Horvath@wolterskluwer.com>
2019-03-26 17:01:42 +0100

    add element implementation

--- module-3/Element.js
+++ module-3/Element.js
@@ -1,1 +1,30 @@
 'use strict';
+
+class Element {
+    constructor(name, locator) {
+        this.locator = locator;
+        this.name = name;
+        this.parent = null;
+        this.children = {};
+    }
+
+    setParent(parent) {
+        this.parent = parent;
+    }
+
+    /**
+     * 
+     * @param {Element} children 
+     */
+    addChildren(children) {
+        if (!children) {
+            throw new Error("There is no children!");
+        }
+        if (this.children.hasOwnProperty(children.name)) {
+            throw new Error(`The children ${children.name} is already defined!`)
+        }
+        this.children[children.name] = children;
+    }
+}
+
+module.exports = Element;
--- module-3/Elements.js
+++ module-3/Elements.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const Element = require("./Element");
+
+class Elements extends Element {
+    constructor(name, locator) {
+        super(name, locator);
+        this.children = null;
+    }
+
+    addChildren() {
+        throw new Error("Elements cannot have children!");
+    }
+}
+
+module.exports = Elements;
