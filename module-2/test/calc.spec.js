18578b9c098165d16e8afd73c0753e0b58f4bed4
yauheni-nestserau <yauheni_nestserau@epam.com>
2019-08-07 10:51:51 +0300

    YN - solution for module 2

--- module-2/test/calc.spec.js
+++ module-2/test/calc.spec.js
@@ -19,19 +19,105 @@ describe.only('calc', () => {
      *     .times(6).v // 24
      */
     // TODO: write test cases to test calculator
-    it ("should have proper value");
+    it ("should have proper value", () => {
+        const c = calc(3);
+        expect(c.v).to.equal(3);
+    });
 
     describe("add", () => {
-        it ("should be able to add a number to the current value");
+        it ("should exist", () => {
+            const c = calc(4);
+            expect(c.add).not.to.be.undefined;
+        });
+
+        it ("should be able to add a number to the current value", () => {
+            const c = calc(3);
+            const result = c.add(5).v;
+            expect(result).to.equal(8);
+        });
     });
 
     describe("minus", () => {
-        it ("should be able to subtract a number from the current value");
+        it ("should exist", () => {
+            const c = calc(5);
+            expect(c.minus).not.to.be.undefined;
+        });
+
+        it ("should be able to subtract a number from the current value", () => {
+            const c = calc(3);
+            const result = c.minus(2).v;
+            expect(result).to.equal(1);
+        });
+    });
+
+    describe("sqrt", () => {
+        it ("should exist", () => {
+            const c = calc(5);
+            expect(c.sqrt).not.to.be.undefined;
+        });
+
+        it ("should be able to extract sqrt the current value", () => {
+            const c = calc(4);
+            const result = c.sqrt(4).v;
+            expect(result).to.equal(2);            
+        });
+
+        it ("should handle sqrt from negative value", () => {
+            const c = calc(-3);
+            expect(() => d.sqrt(-3)).to.throw();           
+        });
+    });
+
+    describe("times", () => {
+        it ("should exist", () => {
+            const c = calc(5);
+            expect(c.times).not.to.be.undefined;
+        });
+
+        it ("should be able to perform times", () => {
+            const c = calc(3);
+            const result = c.times(10).v;
+            expect(result).to.equal(30);            
+        });
     });
 
     describe("divide", () => {
-        it ("should be able to perform a valid division");
-        it ("should handle devision by zero");
+        it ("should exist", () => {
+            const c = calc(42);
+            expect(c.divide).not.to.be.undefined;
+        });
+
+        it ("should be able to perform a valid division", () => {
+            const c = calc(10);
+            const result = c.divide(2).v;
+            expect(result).to.equal(5);
+        });
+
+        it ("should handle devision by zero", () => {
+            const c = calc(5);
+            expect(() => d.divide(0)).to.throw();
+        });
+    });
+
+    describe("modulo", () => {
+        it ("should exist", () => {
+            const c = calc(5);
+            expect(c.modulo).not.to.be.undefined;
+        });
+
+        it ("should be able to perform a valid modulo", () => {
+            const c = calc(10);
+            const result = c.modulo(5).v;
+            expect(result).to.equal(0);
+        });
+    });
+
+    describe("complex operations", () => {
+        it ("should be able to perform a complex operation", () => {
+            const c = calc(3);
+            const result = c.add(4).minus(3).times(6).v;
+            expect(result).to.equal(24);
+        });
     });
 
 
