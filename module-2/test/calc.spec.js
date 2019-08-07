30c7482e7e09b53804f1728767cd9aaf7d55fdc5
SM <malahovskys@gmail.com>
2019-08-08 00:50:09 +0300

    [SM] Complete tasks for module 2

--- module-2/test/calc.spec.js
+++ module-2/test/calc.spec.js
@@ -18,6 +18,95 @@ describe.only('calc', () => {
      *     .minus(3)
      *     .times(6).v // 24
      */
-    // TODO: write test cases to test calculator
+    it("should proper value", () => {
+        const c = calc(3);
+        expect(c.v).to.equal(3);
+    });
+
+    describe("add", () => {
+        it("should exist", () => {
+            const c = calc(3);
+            expect(c.add).not.to.be.undefined;
+        });
+        it("should be able to add a number to the current value", () => {
+            const c = calc(3);
+            const r = c.add(5).v;
+            expect(r).to.equal(8);
+        });
+    });
+
+    describe("minus", () => {
+        it("should exist", () => {
+            const c = calc(3);
+            expect(c.minus).not.to.be.undefined;
+        });
+        it("should be able to subtract a number from the current value", () => {
+            const c = calc(3);
+            const r = c.minus(2).v;
+            expect(r).to.equal(1);
+        });
+    });
+
+    describe("sqrt", () => {
+        it("should exist", () => {
+            const c = calc(3);
+            expect(c.sqrt).not.to.be.undefined;
+        });
+        it("should be able to find a valid square root", () => {
+            const c = calc(4);
+            const r = c.sqrt().v;
+            expect(r).to.equal(2);
+        });
+        it("should handle determination of square root of negative value", () => {
+            const c = calc(-3);
+            expect(() => c.sqrt()).to.throw("Square root of negative value cannot be determined!");
+        });
+    });
+
+    describe("times", () => {
+        it("should exist", () => {
+            const c = calc(3);
+            expect(c.times).not.to.be.undefined;
+        });
+        it("should be able to perform a valid multiplication", () => {
+            const c = calc(3);
+            const r = c.times(10).v;
+            expect(r).to.equal(30);
+        });
+    });
+
+    describe("divide", () => {
+        it("should exist", () => {
+            const c = calc(3);
+            expect(c.divide).not.to.be.undefined;
+        });
+        it("should be able to perform a valid division", () => {
+            const c = calc(10);
+            const r = c.divide(2).v;
+            expect(r).to.equal(5);
+        });
+        it("should handle division by 0", () => {
+            const c = calc(3);
+            expect(() => c.divide(0)).to.throw("Division by 0 is not possible!");
+        });
+    });
+
+    describe("modulo", () => {
+        it("should exist", () => {
+            const c = calc(3);
+            expect(c.modulo).not.to.be.undefined;
+        });
+        it("should be able to find a valid remainder after division", () => {
+            const c = calc(10);
+            const r = c.modulo(5).v;
+            expect(r).to.equal(0);
+        });
+    });
+
+    it("should support chain calls", () => {
+        const c = calc(3);
+        const r = c.add(4).minus(3).times(6).v;
+        expect(r).to.equal(24);
+    });
 
 });
