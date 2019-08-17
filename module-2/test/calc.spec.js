9d64e62c57dcba28c14bd1d0d87e8bd4404bde2d
Katsiaryna Zakharyashchava <Katsiaryna.Zakharyashchava@thomsonreuters.com>
2019-08-18 02:36:17 +0300

    tests module 2

--- module-2/test/calc.spec.js
+++ module-2/test/calc.spec.js
@@ -19,5 +19,106 @@ describe.only('calc', () => {
      *     .times(6).v // 24
      */
     // TODO: write test cases to test calculator
+    describe("add", () => {
+        it("should have proper value", () => {
+            // Given
+            const initialNumber = calc(3);
+            // When
+            // Then
+            expect(initialNumber.v).to.equal(3);
+        });
 
+        it("should be able to add a number to the current value", () => {
+            // Given
+            const initialNumber = calc(3);
+            // When
+            const result = initialNumber.add(5).v;
+            // Then
+            expect(result).to.equal(8);
+        });
+    });
+    
+    describe("minus", () => {
+        it("should be able to add a number to the current value", () => {
+            // Given
+            const initialNumber = calc(3);
+            // When
+            const result = initialNumber.minus(2).v;
+            // Then
+            expect(result).to.equal(1);
+        });
+    });
+
+    describe("sqrt", () => {
+        it("should be able to calculate square root correctly", () => {
+            // Given
+            const initialNumber = calc(4);
+            // When
+            const result = initialNumber.sqrt().v;
+            // Then
+            expect(result).to.equal(2);
+        });
+    
+        it("should handle Square root of negative value", () => {
+            // Given
+            const initialNumber = calc(-3);
+            // When
+            // Then
+            expect(() => initialNumber.sqrt()).to.throw("Square root of negative value cannot be determined!");
+        });
+    });
+
+    describe("times", () => {
+        it("should be able to times correctly", () => {
+            // Given
+            const initialNumber = calc(3);
+            // When
+            const result = initialNumber.times(10).v;
+            // Then
+            expect(result).to.equal(30);
+        });
+    });
+
+    describe("divide", () => {
+        it("should be able to divide correctly", () => {
+
+            // Given
+            const initialNumber = calc(10);
+            // When
+            const result = initialNumber.divide(2).v;
+            // Then
+            expect(result).to.equal(5);
+        });
+    
+        it("should handle division by '0'", () => {
+    
+            // Given
+            const initialNumber = calc(10);
+            // When
+            // Then
+            expect(() => initialNumber.divide(0)).to.throw("Division by 0 is not possible!");
+        });
+    });
+
+    describe("modulo", () => {
+        it("should be able to modulo correctly", () => {
+            // Given
+            const initialNumber = calc(10);
+            // When
+            const result = initialNumber.modulo(5).v;
+            // Then
+            expect(result).to.equal(0);
+        });
+    });
+
+    describe("several function", () => {
+        it("should be able to count correctly", () => {
+            // Given
+            const initialNumber = calc(3);
+            // When
+            const result = initialNumber.add(4).minus(3).times(6).v;
+            // Then
+            expect(result).to.equal(24);
+        });
+    });
 });
