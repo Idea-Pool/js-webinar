bee5dc33dcdcd2496568c65a58ac6f3d180265c4
szikszail <sziklaszlo@gmail.com>
2020-01-13 22:24:48 +0100

    Added async tests

--- plus/async/loadJson.js
+++ plus/async/loadJson.js
@@ -1,13 +1,11 @@
 /*
  * Rewrite the following code to use async/await 
  */
-module.exports = function loadJson(url) {
-    return fetch(url)
-        .then(response => {
-            if (response.status == 200) {
-                return response.json();
-            } else {
-                throw new Error(response.status);
-            }
-        });
+module.exports = async function loadJson(url) {
+    const response = await fetch(url);
+    if (response.status == 200) {
+        return response.json();
+    } else {
+        throw new Error(response.status);
+    }
 };
