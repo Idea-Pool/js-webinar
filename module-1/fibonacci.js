b209964088a79bceb46c86f94d2b8d607e6077a0
Jacek Iwański <jiwanski@users.noreply.github.com>
2020-01-19 22:30:58 +0100

    solution to Fibonacci task

--- module-1/fibonacci.js
+++ module-1/fibonacci.js
@@ -15,7 +15,13 @@ function fibonacci(n) {
      * Also take into consideration the documentation of the function!
      */
     // PLACE YOUR CODE BETWEEN THIS...
-
+    if (n <= 0) {return 0;}
+    // checking for first two (hardcoded) Fibonacci numbers
+    else if ([1, 2].includes(n)) {return 1;}
+    else { // n > 2 should calculate Fibonacci number...
+        // ...so function can call itself
+        nThFibonacci = fibonacci(n - 1) + fibonacci(n - 2)
+    }
     // ...AND THIS COMMENT LINE!
     return nThFibonacci;
 }
