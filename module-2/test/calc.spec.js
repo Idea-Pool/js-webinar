c2b2989b2a8608fba6041aba9ef0f9b484212f2e
Daria Bulkhak <Daria_Bulkhak@epam.com>
2020-01-22 17:28:37 +0200

    Module-2 solutions

--- module-2/test/calc.spec.js
+++ module-2/test/calc.spec.js
@@ -18,4 +18,138 @@ describe.only('calc', () => {
      */
     // TODO: write test cases to test calculator
 
+
+    it ("Should have proper value", () => {
+         const c=calc(3).v;
+         expect(c).equal(3);
+        
+    })
+    describe("add", () => {
+
+        it("Should exist", () =>{
+            const c=calc(3).v;
+            expect(c).not.to.be.undefined;
+
+        })
+        it("Should be able to add a number to the current value", () =>{
+            const c=calc(3);
+            const result=c.add(5).v;
+            expect(result).equal(8);
+
+        })
+
+    })
+
+    describe("minus", () => {
+
+        it("Should exist", () =>{
+            const c=calc(3).v;
+            expect(c).not.to.be.undefined;
+
+        })
+        it("Should be able to subtract a number from the current value", () =>{
+            const c=calc(3);
+            const result=c.minus(2).v;
+            expect(result).equal(1);
+
+        })
+
+    })
+
+    describe("sqrt", () => {
+
+
+        it("Should exist", () =>{
+            const c=calc(4).v;
+            expect(c).not.to.be.undefined;
+
+        })
+        it("Should be able to calculate the square root of the current value", () =>{
+            const c=calc(4);
+            const result=c.sqrt(4).v;
+            expect(result).equal(2);
+
+        })
+        it("Should handle square root of negative value", () => {
+            const c = calc(-3);
+            expect(() => c.sqrt(-3)).throw();
+        });
+
+    })
+
+    describe("times", () => {
+
+        it("Should exist", () =>{
+            const c=calc(3).v;
+            expect(c).not.to.be.undefined;
+
+        })
+        it("Should be able to multiply a number to the current value", () =>{
+            const c=calc(3);
+            const result=c.times(10).v;
+            expect(result).equal(30);
+
+        })
+
+    })
+
+
+    describe("divide", () => {
+
+        it("Should exist", () =>{
+            const c=calc(10).v;
+            expect(c).not.to.be.undefined;
+
+        })
+        it("Should be able to divide a number by the current value", () =>{
+            const c=calc(10);
+            const result=c.divide(2).v;
+            expect(result).equal(5);
+
+        })
+
+        it("Should handle tge divition by zero", () =>{
+            const c = calc(0);
+            expect(() => c.divide(0)).throw();
+
+        })
+
+    })
+
+    describe("modulo", () => {
+
+        it("Should exist", () =>{
+            const c=calc(10).v;
+            expect(c).not.to.be.undefined;
+
+        })
+        it("Should be able to return the remainder of dividing a number by the current value", () =>{
+            const c=calc(10);
+            const result=c.modulo(5).v;
+            expect(result).equal(0);
+
+        })
+
+    })    
+
+    describe("add, minus, times", () => {
+
+        it("Should exist", () =>{
+            const c=calc(3).v;
+            expect(c).not.to.be.undefined;
+
+        })
+        it("Should be able to return result of few operations", () =>{
+            const c=calc(3);
+            const result=c.add(4).minus(3).times(6).v;
+            expect(result).equal(24);
+
+        })
+
+    })
+
+
+
+
+
 });
