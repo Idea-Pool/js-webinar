d0365e98ef49c03bf06536222ab3331b9790ea22
Jacek Iwański <jiwanski@users.noreply.github.com>
2020-02-11 19:28:22 +0100

    solutions to module 2 tasks

--- module-2/test/calc.spec.js
+++ module-2/test/calc.spec.js
@@ -1,4 +1,4 @@
-const calc = require('../calc');
+const calc = require('../calc.js');
 const expect = require('chai').expect;
 
 describe.only('calc', () => {
@@ -17,5 +17,58 @@ describe.only('calc', () => {
      *     .times(6).v // 24
      */
     // TODO: write test cases to test calculator
+    it("should return number", () => {
+        var result = calc(3).v;
+        expect(result).equals(3);
+    });
+
+    it('should add numbers', () => {
+        var result = calc(3).add(5).v;
+        expect(result).equals(8);
+    });
+
+    it('should subtract numbers', () => {
+        var result = calc(3).minus(2).v;
+        expect(result).equal(1);
+    });
+
+    it('should calculate square root', () => {
+        var result = calc(4).sqrt().v;
+        expect(result).equal(2);
+    });
+
+    it('should multiply numbers', () => {
+        var result = calc(3).times(10).v;
+        expect(result).equal(30);
+    });
+
+    it('should divide numbers', () => {
+        var result = calc(10).divide(2).v;
+        expect(result).equal(5);
+    });
+
+    it('should perform modulo on numbers', () => {
+        var result = calc(10).modulo(5).v;
+        expect(result).equal(0);
+    });
+
+    it('should not divide by zero', () => {
+        expect(() => {
+            calc(5).divide(0).v
+        }).to.throw('Division by 0 is not possible!');
+    });
+
+    it('should not calculate square root of negative number', () => {
+        expect(() => {
+            calc(-3).sqrt().v
+        }).to.throw('Square root of negative value cannot be determined!');
+    });
+
+    it('should chain operations', () => {
+        var result = calc(3).add(4).minus(3).times(6).v;
+        expect(result).equal(24);
+    });
+
+
 
 });
