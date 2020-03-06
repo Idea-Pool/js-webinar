1b0bd69865a30c4fdd90b18237578f67b3f2d022
szikszail <sziklaszlo@gmail.com>
2020-03-06 14:06:46 +0100

    Added task description to POP tasks

---
+++ module-3/pop/Elements.js
@@ -0,0 +1,15 @@
+/**
+ * Create Elements class, which represents a collection of
+ * elements of the application, and
+ * 
+ * 1. It has a protractor locator (.locator),
+ *    e.g. by.css("h1.title")
+ * 2. It has a name (.name), e.g. "Document Title"
+ * 3. It can have a parent Element, 
+ *    which is the context of the element (.parent)
+ * 4. It cannot have children elements
+ * 5. It has a method to retrieve all protractor elements
+ *    in the collection by the locator (.all()) in it's context
+ * 6. It has a method to retrieve one element from the collection
+ *    by the locator (.get(n)) in it's context
+ */
---
+++ module-3/pop/Layout.js
@@ -0,0 +1,16 @@
+/**
+ * Create Layout class, which represents a page of
+ * the application, and
+ * 
+ * 1. It has a protractor locator (.locator),
+ *    e.g. by.css("body")
+ * 2. It has a URL (.url), e.g. "/home" or "https://epam.com"
+ * 3. It has a name (.name), e.g. "Document Page"
+ * 4. It cannot have a parent element
+ * 5. It can have children Elements (.children)
+ * 6. It has a method to retrieve the root protractor element
+ *    by the locator (.get([name])) or a child element
+ *    by name in any depth
+ * 7. It has a method to load the page, i.e. Navigates to
+ *    the URL of it (.load())
+ */
