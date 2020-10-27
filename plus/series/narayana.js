19e10a1b0de0ab0124edab654407c56703aa368a
Kalman Takacs <mcmester1987@gmail.com>
2020-10-27 20:39:50 +0100

    fix narayana

--- plus/series/narayana.js
+++ plus/series/narayana.js
@@ -3,7 +3,6 @@
  * Narayana number sequence.
  * https://en.wikipedia.org/wiki/Narayana%27s_cows
  * 
- * @param {number} n (n > k)
- * @param {number} k (k >= 0)
+ * @param {number} n (n >= 0)
  * @returns {number}
  */
