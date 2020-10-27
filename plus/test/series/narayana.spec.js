137221882026005ae38e2de5089d560952e325bb
Kalman Takacs <mcmester1987@gmail.com>
2020-10-27 20:38:52 +0100

    fix tests for narayana cows

--- plus/test/series/narayana.spec.js
+++ plus/test/series/narayana.spec.js
@@ -1,26 +1,29 @@
 const narayana = require("../../series/narayana");
 const expect = require('chai').expect;
 
-describe("In the Narayana series", () => {
-    it("the parameters should be a number", () => {
-        expect(() => narayana("Test", 9)).to.throw(TypeError)
+describe("In the Narayana's cows series", () => {
+    it("the parameter should be a number", () => {
+        expect(()=>narayana("Test")).to.throw(TypeError)
     });
-    it("the negative parameters should return error", () => {
-        expect(() => narayana(-2, 6)).to.throw()
+    it("the -2. number should return error", () => {
+        expect(()=>narayana(-2)).to.throw()
     });
-    it("the negative parameters should return error", () => {
-        expect(() => narayana(3, -1)).to.throw()
+    it("the -1. number should return error", () => {
+        expect(()=>narayana(-1)).to.throw()
     });
-    it("n should be greather then k", () => {
-        expect(() => narayana(2, 6)).to.throw()
+    it("the 0. number should be 1", () => {
+        expect(narayana(0)).to.equal(1);
     });
-    it("the N(6, 3) number should be 50", () => {
-        expect(narayana(6, 3)).to.equal(50);
+    it("the 1. number should be 1", () => {
+        expect(narayana(1)).to.equal(1);
     });
-    it("the N(7, 4) number should be 175", () => {
-        expect(narayana(7, 4)).to.equal(175);
+    it("the 2. number should be 1", () => {
+        expect(narayana(2)).to.equal(1);
     });
-    it("the equivalent parameters should return 1", () => {
-        expect(narayana(2, 2)).to.equal(1);
+    it("the 4. number should be 3", () => {
+        expect(narayana(4)).to.equal(3);
+    });
+    it("the 10. number should be 28", () => {
+        expect(narayana(10)).to.equal(28);
     });
 });
