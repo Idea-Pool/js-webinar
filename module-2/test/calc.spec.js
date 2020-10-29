539320de2d98cc841277c04f4193ac58a8a77547
Tamas Szucs <Tamas_Szucs@epam.com>
2020-10-29 18:04:06 +0100

    Working on module2 exercises

--- module-2/test/calc.spec.js
+++ module-2/test/calc.spec.js
@@ -17,5 +17,86 @@ describe.only('calc', () => {
      *     .times(6).v // 24
      */
     // TODO: write test cases to test calculator
+    
+    // constructor
+    it("should have a proper value", () => {
+        const c = calc(3);
+        expect(c).not.to.be.undefined;
+    });
 
+    describe("add", () => {
+        // Checks If I have a calculator there is an "add" method as well?
+        // If the first IT fails within a describe, the other IT test cases won't be checked
+        it("should exist", () => {
+            // GIVEN
+            const c = calc(3);
+            // WHEN
+            // THEN
+            expect(c.add).not.to.be.undefined;
+        }); 
+
+        it("should be able to add a number to the current value", () => {
+            // GIVEN
+            const c = calc(3);
+            // WHEN
+            const result = c.add(5).v;
+            // THEN
+            expect(result).to.equal(8);
+        });
+    });
+
+    describe("minus", () => {
+        it("should exist", () => {
+            const c = calc(3);
+            expect(c.minus).not.to.be.undefined;
+        }); 
+
+        it("should be able to substract a number from the current value", (asd) => {
+            // GIVEN
+            // WHEN
+            // THEN
+        });
+    });
+
+    describe("square", () => {
+        it("should exist", () => {
+            const c = calc(3);
+            expect(c.sqrt).not.to.be.undefined;
+        }); 
+
+        it("should be able to get the square root of the current value");
+        it("should handle square root with negative number");
+    });
+
+    describe("times", () => {
+        it("should exist", () => {
+            const c = calc(3);
+            expect(c.times).not.to.be.undefined;
+        }); 
+
+        it("should be able to multiply the current value with the given number");
+    });
+
+    describe("divide", () => {
+        it("should exist", () => {
+            const c = calc(3);
+            expect(c.divide).not.to.be.undefined;
+        }); 
+
+        it("should be able to divide the current value with the given number");
+        it("should handle division by 0");
+    });
+
+    describe("modulo", () => {
+        it("should exist", () => {
+            const c = calc(3);
+            expect(c.modulo).not.to.be.undefined;
+        }); 
+
+        it("should be able to get the modulo by dividing the current number with the given number");
+    });
+
+    describe("complexExpression", () => {
+        it("should be able to calculate the given more complex expression");
+    });
 });
