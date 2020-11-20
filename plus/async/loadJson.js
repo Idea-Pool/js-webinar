63d9a68ebe9d1c016e0ca48e573ea9c27c422a87
Tamas Szucs <Tamas_Szucs@epam.com>
2020-11-20 14:38:59 +0100

    Plus/Async exercises solved

--- plus/async/loadJson.js
+++ plus/async/loadJson.js
@@ -1,7 +1,7 @@
 /*
  * Rewrite the following code to use async/await 
  */
-module.exports = function loadJson(url) {
+/* module.exports = function loadJson(url) {
     return fetch(url)
         .then(response => {
             if (response.status == 200) {
@@ -10,4 +10,14 @@ module.exports = function loadJson(url) {
                 throw new Error(response.status);
             }
         });
+}; */
+
+module.exports = async function loadJson(url) {
+    const response = await fetch(url);
+
+    if (response.status === 200) {
+        return response.json();
+    }
+    
+    throw new Error("Request failed (The following error code occured: 500)" , response.status);
 };
--- plus/async/sleep.js
+++ plus/async/sleep.js
@@ -5,3 +5,20 @@
  * @returns {Promise} the promise which resolves when wait if over
  *                    or rejects if parameter is not correct
  */
+
+module.exports = async function sleep(sec) {
+
+    if (typeof sec !== 'number') {
+        throw new Error('The given argument is not a number!', sec);
+    }
+
+    let waitInMilliseconds = sec * 1000;
+    const defaultWaitInMilliseconds = 10000;
+
+    return new Promise((resolve, reject) => {
+        //console.log("waitInMilliseconds: " + waitInMilliseconds);
+        //console.log("defaultWaitInMilliseconds: " + defaultWaitInMilliseconds);
+
+        setTimeout(resolve, Math.min(waitInMilliseconds, defaultWaitInMilliseconds));
+    });
+};
