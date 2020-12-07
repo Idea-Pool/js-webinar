37d12f89f8d73f751928b59986d2bc98a8a90203
Balázs Bakosi <bakosibalazs@gmail.com>
2020-12-07 08:45:26 +0100

    Solutions of module-4 tasks

--- module-4/arraySum.js
+++ module-4/arraySum.js
@@ -6,3 +6,25 @@
  * @param {Array} elements
  * @returns {number} summary of all integers or 0 in other cases
  */
+
+function arraySum(elements) {
+    let paramFlattend;
+    let sum = 0;
+
+    if (typeof (elements) !== "object") {
+        return sum;
+    };
+    if (Array.isArray(elements.flat(Infinity))) {
+        paramFlattend = elements.flat(Infinity)
+    } else {
+        paramFlattend = elements;
+    };
+    paramFlattend.forEach(element => {
+        if (typeof (element) === "number") {
+            sum += element;
+        };
+    });
+    return sum;
+};
+
+module.exports = arraySum;
