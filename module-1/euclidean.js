ab0a3501f0c36414b3bdf59ea9004bee5ca313bb
Márton Gábor <gabor_marton1@epam.com>
2021-05-21 13:21:28 +0200

    add soluiton to euclidean.js

--- module-1/euclidean.js
+++ module-1/euclidean.js
@@ -7,18 +7,20 @@
  * @returns {number} GCD or 0 if any arguments are not proper
  */
 function euclidean(a, b) {
-    let gcd;
-    /*
-     * Your task is to compute the greatest common divisor of
-     * the numbers given in a and b variables, using the
-     * Euclidean algorithm (https://en.wikipedia.org/wiki/Euclidean_algorithm).
-     * If you have the result, assign it to the variable, called gcd.
-     * Also take into consideration the documentation of the function!
-     */
-    // PLACE YOUR CODE BETWEEN THIS...
-
-
-    // ...AND THIS COMMENT LINE!
-    return gcd;
+  let gcd;
+  /*
+   * Your task is to compute the greatest common divisor of
+   * the numbers given in a and b variables, using the
+   * Euclidean algorithm (https://en.wikipedia.org/wiki/Euclidean_algorithm).
+   * If you have the result, assign it to the variable, called gcd.
+   * Also take into consideration the documentation of the function!
+   */
+  // PLACE YOUR CODE BETWEEN THIS...
+  if (a < 0 || b < 0) return 0;
+  if (a === b) gcd = a;
+  else if (a > b) gcd = euclidean(a - b, b);
+  else if (a < b) gcd = euclidean(a, b - a);
+  // ...AND THIS COMMENT LINE!
+  return gcd;
 }
 module.exports = euclidean;
