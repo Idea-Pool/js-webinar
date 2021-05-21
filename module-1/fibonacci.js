e0570d320d7a60b9f5c6a0e1a9527fe637871d44
Márton Gábor <gabor_marton1@epam.com>
2021-05-21 13:22:07 +0200

    add solution to fibonacci.js

--- module-1/fibonacci.js
+++ module-1/fibonacci.js
@@ -6,17 +6,23 @@
  * @returns {number} Fibonacci number or 0 if any arguments are not proper
  */
 function fibonacci(n) {
-    let nThFibonacci;
-    /*
-     * Your task is to calculate the nth value of the
-     * Fibonacci sequence.
-     * https://en.wikipedia.org/wiki/Fibonacci_number
-     * Store the value in the nThFibonacci variable.
-     * Also take into consideration the documentation of the function!
-     */
-    // PLACE YOUR CODE BETWEEN THIS...
+  let nThFibonacci;
+  /*
+   * Your task is to calculate the nth value of the
+   * Fibonacci sequence.
+   * https://en.wikipedia.org/wiki/Fibonacci_number
+   * Store the value in the nThFibonacci variable.
+   * Also take into consideration the documentation of the function!
+   */
+  // PLACE YOUR CODE BETWEEN THIS...
+  nThFibonacci = ((n) => {
+    if (n < 0) return 0;
+    if (n === 1) return 1;
+    if (n === 2) return 1;
 
-    // ...AND THIS COMMENT LINE!
-    return nThFibonacci;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  })(n);
+  // ...AND THIS COMMENT LINE!
+  return nThFibonacci;
 }
 module.exports = fibonacci;
