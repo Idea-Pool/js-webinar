5e314118eacfc487fd71a52af8049e36868d1018
Gergely Kadar <gergely_kadar@epam.com>
2022-07-05 13:26:24 +0200

    solved tasks

--- module-1/classification.js
+++ module-1/classification.js
@@ -12,6 +12,7 @@
  */
 function grade(score) {
     let gradeOfStudent;
+   
     /*
      * Your task is to calculate the grade of the student
      * based on his/her score which can be found in the
@@ -21,8 +22,30 @@ function grade(score) {
      * Also take into consideration the documentation of the function!
      */
     // PLACE YOUR CODE BETWEEN THIS...
-
+    
+    if (score <= 100 && score>= 90){
+         gradeOfStudent = 5;
+    } else if (score >= 80 && score <= 89){
+         gradeOfStudent = 4;
+    } else if (score >= 70 && score <= 79){
+          gradeOfStudent = 3;
+    } else if (score >= 60 && score <= 69){
+         gradeOfStudent = 2;
+    } else if (score >= 0 && score <= 59){
+         gradeOfStudent = 1;
+    } else {
+        gradeOfStudent = 0;
+    }
     // ...AND THIS COMMENT LINE!
-    return gradeOfStudent;
+   return gradeOfStudent
+
 }
 module.exports = grade;
+
+console.log(grade(77));
+
+
+
+
+
+ 
